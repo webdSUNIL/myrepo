fb97f1c722f24d7083d2f8f61d19ef9744bfa911
webdSUNIL <sunilpatra283@gmail.com>
2022-12-29 18:37:18 +0530

    git remote

---
+++ myrepo/operators1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int num1=6;
+    int num2=3;
+
+    cout <<num1+num2<<endl;
+
+    cout <<num1-num2<<endl;
+    cout <<num1*num2<<endl;
+    cout <<num1/num2<<endl;
+     cout <<num1%num2<<endl;
+
+
+    cout <<(num1==num2) <<endl;
+    cout <<(num1 != num2)<<endl;
+    cout <<(num1 >= num2)<<endl ;
+
+    int c=6;
+    cout<<(c++)<<endl;
+    cout<< c <<endl;
+
+    int b=c+5;
+    cout<<(b)<<endl;
+
+    
+
+    return 0;
+
+}
